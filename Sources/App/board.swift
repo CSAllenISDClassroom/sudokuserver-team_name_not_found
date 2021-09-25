8529972f98ff4692ea5c7d01739c9f5a763af49e
Garrett Fundis <garrett.fundis@student.allenisd.org>
2021-09-25 10:50:07 -0500

    Moved the Board.swift into the SudokuServer directory Moved several functions from board.swift to routes.swift Changed testboard to incorporate it into the app.get

---
+++ Sources/App/board.swift
@@ -0,0 +1,203 @@
+// Program by Garrett Fundis, Enzo Norton-Mitchell, William Jackson, Samuel Steele
+
+
+
+
+
+/*
+
+ Task 1: Printing layout correctly
+   // Subs-square visual seapartions of a 9x9 into 3
+
+ Task 2: Valitdation
+   // Row iteration: 1 - 9 only
+   // Column iteration: 1 - 9 only
+   // 3x3 subsquare: 1- 9 only
+
+*/
+
+/*
+
+ For Loops & Control Flows
+  https://docs.swift.org/swift-book/LanguageGuide/ControlFlow.html
+  https://www.avanderlee.com/swift/loops-swift/
+  https://learnappmaking.com/swift-where-how-to/
+
+ Collections
+  https://docs.swift.org/swift-book/LanguageGuide/CollectionTypes.html
+
+ Set Documentation
+  https://developer.apple.com/documentation/swift/set
+
+*/
+
+import Foundation
+
+public class Board {
+
+    struct Constants {
+        static let one: Character =   "1"
+        static let two: Character =   "2"
+        static let three: Character = "3"
+        static let four: Character =  "4"
+        static let five: Character =  "5"
+        static let six:  Character =  "6"
+        static let seven: Character = "7"
+        static let eight: Character = "8"
+        static let nine: Character =  "9"
+    }
+
+    public var tiles: [[Character]] = [[Character]]()
+
+
+    public func isArrayValid(_ numbers: [Int]) -> Bool {
+        // Only unqiue and valid #s
+        return hasValidNumbers(numbers) && onlyUniqueNumbers(numbers)
+    }
+
+    // TODO handle processing character number
+    func hasValidNumbers(_ numbers: [Int]) -> Bool {
+        // Assuming tiles is populated
+
+        // Loop through every number in "row" checking to see if it is oneToNine“
+        for value in numbers where isValid(value) != true {
+            return false // If not return false
+        }
+        return true
+    }
+
+    func onlyUniqueNumbers(_ numbers: [Int]) -> Bool {
+        // Assuming tiles is populated
+        // Row iteration: 1 - 9 only - for
+        var alreadySeen = Set<Int>()
+
+        // Check 1.5: Make sure there are no repeating nubers
+        for value in numbers {
+            if alreadySeen.contains(value) {
+                // Value is not unqiue / a value repeated in the row
+                return false
+            }
+            else {
+                // Value is unquie up this point in the array
+                alreadySeen.insert(value)
+            }
+        }
+        return true
+    }
+
+
+    // Check 1: Make sure its within the range (1 - 9)
+    func isValid(_ number: Int) -> Bool {
+        return number >= 1 && number <= 9
+    }
+
+    init() {
+        //creates a valid sudoku board
+        let offsets = [0, 3, 6, 1, 4, 7, 2, 5, 8]
+        for y in 0 ..< 9 {
+            tiles.append([Character]())
+            for x in 0 ..< 9 {
+                var tileValue = x + 1 + offsets[y]
+                while tileValue > 9 {
+                    tileValue -= 9
+                }
+                tiles[y].append(Character("\(tileValue)"))
+            }
+        }
+
+       //randomize the board
+        var characters : [Character] = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        characters.shuffle()
+        for y in 0 ..< tiles.count {
+            for x in 0 ..< tiles[y].count {
+                //if Int.random(in:1...10) > 7 {
+                switch tiles[y][x] {
+                case "1":
+                    tiles[y][x] = characters[0]
+                case "2":
+                    tiles[y][x] = characters[1]
+                case "3":
+                    tiles[y][x] = characters[2]
+                case "4":
+                    tiles[y][x] = characters[3]
+                case "5":
+                    tiles[y][x] = characters[4]
+                case "6":
+                    tiles[y][x] = characters[5]
+                case "7":
+                    tiles[y][x] = characters[6]
+                case "8":
+                    tiles[y][x] = characters[7]
+                case "9":
+                    tiles[y][x] = characters[8]
+                default:
+                    fatalError("invalid character found in Board")
+                }
+            }
+            // else {
+            //   tiles[y][x] = " "
+            //}
+        }
+        
+    } 
+}
+
+
+
+/*let falseBoard = Board()
+falseBoard.tiles = [[Board.Constants.two, Board.Constants.four, Board.Constants.six, Board.Constants.eight]]
+
+testBoard(falseBoard)
+*/
+//test
+
+//testBoard(board)
+ 
+
+
+
+
+
+//In Progress (In the process of creating a random array using the randomElement()! that works with tiles.)
+
+//randomize the board
+
+/*
+ var characters : [Character] = ["1", "2", "3", "4", "5", "6", "7", "8", "9"] 
+        guard characters.count > 0 else {
+            return
+        }
+        let randomNumber = characters.randomNumber()!
+        for y in 0 ..< tiles.count {
+            for x in 0 ..< tiles[y].count {
+                //if Int.random(in:1...10) > 7 {
+                switch tiles[y][x] {
+                case "1":
+                    tiles[y][x] = randomNumber[0]
+                case "2":
+                    tiles[y][x] = randomNumber[1]
+                case "3":
+                    tiles[y][x] = randomNumber[2]
+                case "4":
+                    tiles[y][x] = randomNumber[3]
+                case "5":
+                    tiles[y][x] = randomNumber[4]
+                case "6":
+                    tiles[y][x] = randomNumber[5]
+                case "7":
+                    tiles[y][x] = randomNumber[6]
+                case "8":
+                    tiles[y][x] = randomNumber[7]
+                case "9":
+                    tiles[y][x] = randomNumber[8]
+                default:
+                    fatalError("invalid character found in Board")
+                }
+            }
+            // else {
+            //   tiles[y][x] = " "
+            //}
+        }
+    }
+
+*/
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,53 @@
 import Vapor
+import Foundation
+
+struct ResponseData : Content {
+    var action: String
+    var payload: String
+    var response: String
+    var statusCode: String
+    }
 
 func routes(_ app: Application) throws {
     app.get { req in
         return "It works!"
     }
 
-    app.get("hello") { req -> String in
+    app.get("hello") { req in
         return "Hello, world!"
     }
+
+    app.get("games", ":id", "cells") { req -> String in
+        //guard let id = req.parameters.get("id", as: Int.self) else {
+        //    throw Abort(.badRequest)
+        //}
+        return "\(testBoard(board))"
+    }.description("200 OK")
+
+    
+}
+
+func testBoard(_ board: Board) -> String {
+    var finishedBoard = ""
+    for row in board.tiles   {
+        let toIntArray = createIntegerArray(row)
+        let valid = board.isArrayValid(toIntArray)
+        finishedBoard += "\(row): \(valid)"
+    }
+    return finishedBoard
+}
+
+let board = Board()
+
+func createIntegerArray(_ array: [Character]) -> [Int] {
+    var intArray = [Int]()
+    for char in array {
+        let toString = String(char)
+        guard let toInt = Int(toString) else {
+            print("Error: Cannot Cast \(toString) to an integer")
+            return intArray
+        }
+        intArray.append(toInt)
+    }
+    return intArray
 }
