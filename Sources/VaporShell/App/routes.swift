3f271bc036df7f6fcd7e9bca0e990c7ce6b74196
Garrett Fundis <garrett.fundis@student.allenisd.org>
2021-10-22 14:34:32 -0500

    Added descriptions to badRequests

--- Sources/VaporShell/App/routes.swift
+++ Sources/VaporShell/App/routes.swift
@@ -25,17 +25,17 @@ func routes(_ app: Application) throws {
     //returns ID for a new board
     app.post("games") { req -> String in
         boards.append(Board())
-        
+
         return "{\"id\": \(boards.count-1)}"
-    }
+    }.description("201 Created")
 
     //returns cells from given id in json format
     app.get("games", ":id", "cells") { req -> String in
         guard let id = req.parameters.get("id", as: Int.self) else {
-            throw Abort(.badRequest)
+            throw Abort(.badRequest, reason: ("Your ID does not consist of only numbers."))
         }
         if id >= boards.count {
-            throw Abort(.badRequest)
+            throw Abort(.badRequest, reason: ("This ID has not been created yet."))
         }
         guard let data = try? encoder.encode(board),
               let json = String(data: data, encoding: .utf8) else {
@@ -45,30 +45,28 @@ func routes(_ app: Application) throws {
     }.description("200 OK")
 
     //places specified value at given y and x
-    app.put("games", ":id", "cells", ":y", ":x", ":value") { req -> String in
+    app.put("games", ":id", "cells", ":cellIndex", ":boxIndex", ":value") { req -> String in
         guard let id = req.parameters.get("id", as: Int.self) else {
-            throw Abort(.badRequest)
+            throw Abort(.badRequest, reason: ("The ID you entered does not exist."))
         }
-        guard let y = req.parameters.get("y", as: Int.self) else {
-            throw Abort(.badRequest)
+        guard let y = req.parameters.get("cellIndex", as: Int.self) else {
+            throw Abort(.badRequest, reason: ("cellIndex is out of range 0 ... 8"))
         }
-        guard let x = req.parameters.get("x", as: Int.self) else {
-            throw Abort(.badRequest)
+        guard let x = req.parameters.get("boxIndex", as: Int.self) else {
+            throw Abort(.badRequest, reason: ("boxIndex is out of range 0 ... 8"))
         }
         guard let value = req.parameters.get("value", as: Int.self) else {
-            throw Abort(.badRequest)
+            throw Abort(.badRequest, reason: ("value must be an integer"))
         }
         if y > 8 || y < 0 || x > 8 || x < 0 || value > 9 || value < 1 {
-            throw Abort(.badRequest)
+            throw Abort(.badRequest, reason: ("value is out of range 1 ... 9 or null"))
         }
         if id >= boards.count {
-            throw Abort(.badRequest)
+            throw Abort(.badRequest, reason: ("This ID has not been created yet."))
         }
-
-        //do stuff here i, will, will fix it later
         
         return ""
-    }
+    }.description("200 OK")
 }
 /* this isn't needed right now, it should return a boolean once we add it back
 func testBoard(_ board: Board) -> String {
